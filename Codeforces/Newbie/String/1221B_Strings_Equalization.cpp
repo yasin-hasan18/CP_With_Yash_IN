d8260f66d32e991e376b7282653bf1f4626dd847
Yasin Hasan(Sabbir) <133748773+yasin-hasan18@users.noreply.github.com>
2026-03-14 16:30:25 +0600

    Codeforces solution ( Strings *1000)

--- Codeforces/Newbie/String/1221B_Strings_Equalization.cpp
+++ Codeforces/Newbie/String/1221B_Strings_Equalization.cpp
@@ -10,36 +10,30 @@ using ll = long long;
 #define frr(w,n) for(int i=w; i<n; i++)
 
 void faaa() {
-    string s1, s2;
-    getline(cin, s1);
-    getline(cin, s2);
+    string s, t;
+    cin >> s >> t;
 
-    unordered_map<char,int> fr;
-    for(char c : s1) {
-        if(c != ' ') fr[c]++;
-    }
+    set<char> sts(s.begin(), s.end());
+    set<char> stt(t.begin(), t.end());
 
-    bool pos = true;
-    for(char c : s2) {
-        if(c == ' ') continue;
-        if(fr[c] > 0) {
-            fr[c]--;
-        } else {
-            pos = false;
+    bool pos = false;
+    for(char c : sts) {
+        if(stt.count(c)) {
+            pos = true;
             break;
         }
     }
 
-    cout << (pos ? "YES" : "NO") << "\n";
-}
+        cout << (pos ? "YES" : "NO") << "\n";
+    }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    // int t;
-    // cin >> t;
-    // while (t--)
+    int t;
+    cin >> t;
+    while (t--)
         faaa();
 
     return 0;
